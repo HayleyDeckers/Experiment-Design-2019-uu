432b7a65a8045d73514e2a148192d4561ec05b7e
Hayley <github.com@codingcat.nl>
2019-10-09 19:41:18 +0200

    Improved arduino-python interface

--- src/AnalogReadSerial.ino
+++ src/AnalogReadSerial.ino
@@ -10,29 +10,69 @@
   http://www.arduino.cc/en/Tutorial/AnalogReadSerial
 */
 
+
+//used for handshaking
+String inputString = "";         // a String to hold incoming data
+bool handshakeComplete = false;  // whether the string is complete
+
+#define BUFFER_SIZE 2048
+uint16_t buffer[BUFFER_SIZE];
+uint16_t buffer_head = 0;
+
 // the setup routine runs once when you press reset:
 void setup() {
+  //reserve some space for our string.
+  inputString.reserve(200);
   //increase the analogread resolution because we use a Due
   analogReadResolution(12);
   // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
-
+  Serial.begin(115200);
+  //Wait on the serial interface to be connected
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
 }
 
 uint16_t output = 0;
-
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
-  uint16_t sensorValue = analogRead(A0);
-  output = 3*(output/4)+sensorValue/4;
-  //uint16_t flush_val = 0xffff;
-  //uint16_t write_val[] = {sensorValue, flush_val};
-  //sensorValue++;
-  // print out the value you read:
-  //Serial.write((const char*)&write_val,sizeof(write_val));
-  Serial.println(output);
-  //Serial.flush();
-  //Serial.println(sensorValue);
-  delay(1);        // delay in between reads for stability
+  if(handshakeComplete){
+    // read the input on analog pin 0:
+    uint16_t sensorValue = analogRead(A0);
+    //put the result into a buffer
+    buffer[buffer_head++] = sensorValue;
+    if(buffer_head >= BUFFER_SIZE){
+      Serial.write((const char*)buffer, sizeof(buffer[0])*BUFFER_SIZE);
+      buffer_head = 0;
+    }
+    delay(1); // delay in between reads for stability.
+    //n.b. for an iron sample, of around 10cm, a 10ms delay between
+    // the coefficient of expansion of iron is around 15e-6 per Kelvin.
+    // for a 10cm rod of iron that mean an expansion of 15e-7 m/K
+    // we can measure half-wavelengths with our setup, if we measure once per 100ms (= 10Hz = much slower than our electronics)
+    // that means the maximum expansion we can measure is 3.25e-6 m/s
+    // Combining this we get that the maximum change in temprature per time we could measure is 2.16 K/s.
+    // so a delay of 10ms will be more than short enough to get useful data.
+  } else {
+    while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    //Serial.println(inputString);
+    //TODO: use a commands enum.
+    if (inChar == '\n') {
+      if(inputString.equals("MITE\n")){
+        Serial.write("ACKN");
+        handshakeComplete = true;
+      }else{
+        inputString = "";
+      }
+    }
+  }
+  }
+
 }
--- src/analogRead.py
+++ src/analogRead.py
@@ -8,42 +8,30 @@ n_points = 100;
 x = np.linspace(0, n_points, n_points)
 y = np.zeros(n_points)
 
-# You probably won't need this if you're embedding things in a tkinter plot...
+# plot stuff
 plt.ion()
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.set_ylim(0,2**16)
+ax.set_ylim(500,1200)
 line1, = ax.plot(x, y, 'r-') # Returns a tuple of line objects, thus the comma
 
-
 COM = '/dev/ttyACM1'# (Linux)
-BAUD = 9600
-
-ser = serial.Serial(COM, BAUD, timeout = .1)
-
+BAUD = 115200
+ser = serial.Serial(COM, BAUD, timeout = 5)
 print('Waiting for device');
-sleep(3)
 print(ser.name)
-
 i = 0;
-
+ser.write("MITE\n".encode('ascii'))#
+preface = ser.read_until(b'ACKN')
+print("got preface: ", preface)
 while True:
-    #raw = ser.read(2) #read 2 bytes
     raw = ser.read_until(b'\xff\xff', 4096)
-    line = ser.readline()
-    print(raw)
     if len(raw) == 4:
-        #line = ser.readline()
-    #as_int = int(raw.decode().strip("\r\n"))
-    #print(int.from_bytes(raw, byteorder='big', signed=False))
         data_part = raw[0:2]
         as_le = int.from_bytes(data_part, byteorder='little', signed=False)
-        as_be = int.from_bytes(data_part, byteorder='big', signed=False)
-        #print(as_int) #convert them to an integer and flush
-        print(data_part, as_le, as_be)
-        print(line)
-        y[i] = as_le
-        i = (i + 1)%100
+        print(as_le)
+        y = np.roll(y, -1)
+        y[n_points-1] = as_le
         line1.set_ydata(y)
         fig.canvas.draw()
         fig.canvas.flush_events()
