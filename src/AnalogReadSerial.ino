dcb77da10ca206a31a6af7cd1ba5fa6fd1f85e17
Hayley <github.com@codingcat.nl>
2019-10-21 04:48:32 +0200

    Simplify code, only batch mode

--- src/AnalogReadSerial.ino
+++ src/AnalogReadSerial.ino
@@ -1,17 +1,12 @@
-/*
-  AnalogReadSerial
-*/
-
-
 //used for handshaking
 String inputString = "";         // a String to hold incoming data
 bool handshakeComplete = false;  // whether the string is complete
-bool realtime = false;
 
+//A
 typedef struct DataPacket{
   uint16_t photosensor;
   uint16_t thermocouple;
-  uint32_t timestamp;//TODO add _in_ms
+  uint32_t timestamp_in_ms;
 }DataPacket_t;
 
 #define BUFFER_SIZE 4096/sizeof(DataPacket)
@@ -22,73 +17,58 @@ uint16_t buffer_head = 0;
 void setup() {
   //reserve some space for our string.
   inputString.reserve(200);
-  //increase the analogread resolution because we use a Due
-  //analogReadResolution(12);
   // initialize serial communication at 115200 bits per second:
   Serial.begin(115200);
   //Wait on the serial interface to be connected
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
+  while(!handshakeComplete){
+    while (Serial.available()) {
+      // get the new byte:
+      char inChar = (char)Serial.read();
+      //TODO: check that string doesn't grow beyond 200 bytes
+      // add it to the inputString:
+      inputString += inChar;
+      //TODO: use a commands enum instead
+      // a newline character '\n' is used as a command delimiter
+      if (inChar == '\n') {
+        if(inputString.equals("THERMITE\n")){
+          //Write "ACKN" so that the other end knows that we are ready to start sending data.
+          // sometimes a serial buffer might still contain some random noise, this also helps get rid of that.
+          Serial.write("ACKN");
+          handshakeComplete = true;
+        }else{
+          //if we didn't get any commands, clear the buffer.
+          inputString = "";
+        }
+      }
+    }
+  }
 }
 
-uint16_t output = 0;
 DataPacket_t measurement;
 // the loop routine runs over and over again forever:
 void loop() {
-  if(handshakeComplete){
+  measurement.photosensor = analogRead(A0);
+  measurement.timestamp_in_ms = millis();
+  measurement.thermocouple = analogRead(A1);
 
-    measurement.photosensor = analogRead(A0);
-    measurement.timestamp = millis();
-    measurement.thermocouple = analogRead(A1);
-
-    //put the result into a buffer
-    if(!realtime){
-      buffer[buffer_head] = measurement;//(output/4)*3 +sensorValue/4;
-      buffer_head += 1;
-      if(buffer_head >= BUFFER_SIZE){
-        Serial.write((const char*)buffer, sizeof(measurement)*BUFFER_SIZE);
-        buffer_head = 0;
-      }
-    }else{
-      //TODO: send multiple values in realtime mode
-      uint16_t write_val[] = {analogRead(A0), 0xffff};
-      Serial.write((const char*)&write_val,4);
-    }
-    delay(1); // delay in between reads for stability.
-    //n.b. for an iron sample, of around 10cm, a 10ms delay between
-    // the coefficient of expansion of iron is around 15e-6 per Kelvin.
-    // for a 10cm rod of iron that mean an expansion of 15e-7 m/K
-    // we can measure half-wavelengths with our setup, if we measure once per 100ms (= 10Hz = much slower than our electronics)
-    // that means the maximum expansion we can measure is 3.25e-6 m/s
-    // Combining this we get that the maximum change in temprature per time we could measure is 2.16 K/s.
-    // so a delay of 1ms will be more than short enough to get useful data.
-  } else {
-    while (Serial.available()) {
-    // get the new byte:
-    char inChar = (char)Serial.read();
-
-    //TODO: check that string doesn't grow beyond 200 bytes
-    // add it to the inputString:
-    inputString += inChar;
-    // if the incoming character is a newline, set a flag so the main loop can
-    // do something about it:
-    //Serial.println(inputString);
-    //TODO: use a commands enum.
-    if (inChar == '\n') {
-      if(inputString.equals("REALTIME\n")){
-        Serial.write("ACKN");
-        realtime = true;
-        handshakeComplete = true;
-      }else if(inputString.equals("BATCH\n")){
-        Serial.write("ACKN");
-        realtime = false;
-        handshakeComplete = true;
-      }else{
-        inputString = "";
-      }
-    }
+  buffer[buffer_head] = measurement;
+  buffer_head += 1;
+  //if the buffer is now full
+  if(buffer_head >= BUFFER_SIZE){
+    //flush the buffer
+    Serial.write((const char*)buffer, sizeof(measurement)*BUFFER_SIZE);
+    //and reset the head
+    buffer_head = 0;
   }
-  }
-
+  delay(1); // delay in between reads for stability.
+  //n.b. for an iron sample, of around 10cm, a 10ms delay between
+  // the coefficient of expansion of iron is around 15e-6 per Kelvin.
+  // for a 10cm rod of iron that mean an expansion of 15e-7 m/K
+  // we can measure half-wavelengths with our setup, if we measure once per 100ms (= 10Hz = much slower than our electronics)
+  // that means the maximum expansion we can measure is 3.25e-6 m/s
+  // Combining this we get that the maximum change in temprature per time we could measure is 2.16 K/s.
+  // so a delay of 1ms will be more than short enough to get useful data.
 }
--- src/analogRead.py
+++ src/analogRead.py
@@ -1,57 +1,40 @@
 import serial
 from time import sleep
-import sys
-import matplotlib.pyplot as plt
-import numpy as np
-from struct import unpack
 from struct import iter_unpack
 
+PACKET_SIZE = 8
+BYTES_PER_FRAME = 4096
+PACKETS_PER_FRAME = BYTES_PER_FRAME/PACKET_SIZE
 #TODO: make this a command line option
-#BUG: plotting is too slow for real-time mode.
-use_batch = True
+#NOTE: this has to be changed to "COM1" on Windows!
+#the number might also vary per pc.
 COM = '/dev/ttyACM1'# (Linux)
 BAUD = 115200
+#connect to the arduino over serial
 ser = serial.Serial(COM, BAUD, timeout = 5)
+#sleep for a little while so that we can actuall connect
 sleep(3)
-ser.write(("BATCH\n" if use_batch else "REALTIME\n").encode('ascii'))
+#send an identification payload
+ser.write("THERMITE\n".encode('ascii'))
+# and clear the read buffer up-to-and-including acknowledge from the Arduino
 preface = ser.read_until(b'ACKN')
-print("got preface: ", preface)
-
-if not use_batch:
-    n_points = 100;
-    x = np.linspace(0, n_points, n_points)
-    y = np.zeros(n_points)
-
-    # plot stuff
-    plt.ion()
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_ylim(500,1200)
-    line1, = ax.plot(x, y, 'r-') # Returns a tuple of line objects, thus the comma
-
-    while True:
-        raw = ser.read_until(b'\xff\xff', 4096)
-        if len(raw) == 4:
-            data_part = raw[0:2]
-            as_le = int.from_bytes(data_part, byteorder='little', signed=False)
-            print(as_le)
-            y = np.roll(y, -1)
-            y[n_points-1] = as_le
-            line1.set_ydata(y)
-            fig.canvas.draw()
-            fig.canvas.flush_events()
-        else:
-            print("SAW LENGTH", len(raw))
-else:
-    f = open('out.bin', 'wb')
-    while True:
-        #TODO: remove hardcoded size and format
-        #TODO: deal with not reading exactly 4096 bytes, won't corrupt data as iter_unpack will fail in that case but needs a nice solution.
-        # easy solution would be to just round down the len. Better solution would be to packet the data.
-        data = ser.read(4096)
-        print(len(data))
-        y = iter_unpack("HHI",data)
-        for measurement in y:
-            print(measurement)
-        f.write(data)
-    f.close()
+#now open a binary file to store our data into
+f = open('out.bin', 'wb')
+while True:
+    #TODO: deal with not reading exactly 4096 bytes, won't corrupt data as iter_unpack will fail in that case but needs a nice solution.
+    # easy solution would be to just round down the len. Better solution would be to packet the data.
+    data = ser.read(BYTES_PER_FRAME)
+    #read some data, the math and print statements are to watch progress on the terminal.
+    sum_t = 0.0
+    sum_i = 0.0
+    y = iter_unpack("HHI",data)
+    for measurement in y:
+        sum_i += measurement[0]
+        sum_t += measurement[1]
+    sum_i /= PACKETS_PER_FRAME
+    sum_t /= PACKETS_PER_FRAME
+    #prints smoothed out data
+    print(sum_t, " ", sum_i)
+    #and copy the raw data to a file.
+    f.write(data)
+f.close()
